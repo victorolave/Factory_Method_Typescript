3915e3b31ccbde2ebd6ae63539e9f6625e2dfc1b
Victor Olave <victorolave1131@gmail.com>
2019-08-11 00:05:45 -0500

    Correction

--- Typescript/LoggerTest.ts
+++
@@ -1,6 +0,0 @@
-import { LoggerFactory } from '../loggerFactoryy';
-
-const loggerFactory = new LoggerFactory();
-
-const Console = loggerFactory.getLogger({message: 'Bye World', type: 'File'});
-Console.log();
